9ff5b2d5f642d42900b96d8e3ad84bc3931ce6d7
avaldin <valdinalan.pro@gmail.com>
2024-02-21 11:38:33 +0100

    starting the project

---
+++ philo/header/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avaldin <avaldin@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 11:32:07 by avaldin           #+#    #+#             */
+/*   Updated: 2024/02/21 11:36:31 by avaldin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef	struct s_data
+{
+
+};
+
+
+#endif
